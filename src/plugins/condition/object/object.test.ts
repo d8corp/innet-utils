b5bb953f34debc49b34c9212e34494ce03236d6e
Deight <d8corp@mail.ru>
2026-07-04 00:44:56 +0300

    Add test

--- src/plugins/condition/object/object.test.ts
+++ src/plugins/condition/object/object.test.ts
@@ -1,4 +1,4 @@
-import { createHandler, innet } from 'innet'
+import { createHandler, innet, NEXT } from 'innet'
 
 import { createLogger } from '../../../testUtils'
 import { object } from '.'
@@ -22,5 +22,26 @@ describe('object', () => {
     innet(null, handler)
     innet(new Set(), handler)
     expect(log).toBeCalledTimes(4)
+  })
+  it('runs for any object with extends', () => {
+    const log: any[] = []
+
+    const handler1 = createHandler([
+      object([() => () => {
+        log.push('A')
+        return NEXT
+      }]),
+    ])
+
+    const handler2 = createHandler([
+      object([() => () => {
+        log.push('B')
+        return NEXT
+      }]),
+    ], handler1)
+
+    innet({}, handler2)
+
+    expect(log).toEqual(['B', 'A'])
   })
 })
